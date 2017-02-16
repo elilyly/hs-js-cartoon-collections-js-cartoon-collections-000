fdca43b1113d11e5a047aa68eb00c01d4da1f4dd
Elizabeth Nicholson <elizabethnich19@gmail.com>
2017-02-16 05:02:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
+
 function dwarfRollCall(dwarves) {
+  var line = [];
+  for (var i = 0; i < dwarves.length; i++) {
+    var list = ((i + 1) + '. ' + dwarves[i]);
+    line.push(list);
+  }
+    return line.join(" ") + " ";
 }
 
-function summonCaptainPlanet(planeteerCalls){
+function summonCaptainPlanet(veggies){
+  var arr = [];
+  for (var i = 0; i < veggies.length; i++) {
+    var veg = veggies[i].toUpperCase() + "!";
+    arr.push(veg);
+  }
+    return arr;
 }
 
 function longPlaneteerCalls(words) {
+for (var i = 0; i < words.length; i++) {
+    if (words[i] < words[i + 4]) {
+    return true;
+    } else {
+    return false;
+  }
+ }
 }
 
 function findTheCheese (foods) {
+  for (var i = 0; i < foods.length; i++) {
+    if (foods[i] === 'cheddar') {
+      return 'cheddar';
+    } else if (foods[i] === 'gouda') {
+      return 'gouda';
+    } else if (foods[i] === 'camembert') {
+      return 'camembert';
+    }
+  } return "no cheese!";
 }
