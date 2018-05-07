952ff28e6ca0e1f23473a813f38390d64168ff24
Elizabeth Nicholson <elizabethnich19@gmail.com>
2018-05-07 01:25:10 +0000

    automatically backed up by learn

--- index2.js
+++ index2.js
@@ -1,7 +1,7 @@
 function RollCall(names) {
   var str =  "";
   for (i=0; i < names.length; i++) {
-    str = `${str}${i+1} ${names[i]} `
+    str = `${str}${i+1}. ${names[i]} `
   }
   return str
 }
